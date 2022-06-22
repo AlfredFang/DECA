4a0518c4318c12333ec85c604d3d221493e5c98a
AlfredFang <1250585848@qq.com>
2022-06-22 13:37:16 +0800

    Changes

---
+++ run_demo.sh
@@ -0,0 +1,3 @@
+python demos/demo_reconstruct.py -i TestSamples/test -s TestSamples/test/reconstruct_results
+python demos/demo_transfer.py -i TestSamples/test -e TestSamples/exp/7.jpg -s TestSamples/test/transfer_results
+python demos/demo_teaser.py -i TestSamples/test -e TestSamples/exp -s TestSamples/test/teaser_results
